501fbda1378e6472c05e9dab023630bc6b78cfd1
big <thitiphong479big@hotmail.com>
2026-01-13 20:51:28 +0700

    add shadcn UI

--- warehouse/app/page.tsx
+++ warehouse/app/page.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
 }
 
 export default Home;
