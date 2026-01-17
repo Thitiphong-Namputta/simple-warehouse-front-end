9308bf4211f6d86fb4fb99784afd4a66a9b23fcb
big <thitiphong479big@hotmail.com>
2026-01-17 21:10:13 +0700

    add bar chart and updated grid layout dashboard page

--- warehouse/app/warehouse/page.tsx
+++ warehouse/app/warehouse/page.tsx
@@ -3,15 +3,27 @@
 import { AppHeader } from "@/components/layouts/app-header";
 import { LineChart } from "@/components/chart/line";
 import { AreaChart } from "@/components/chart/area";
+import { BarChart } from "@/components/chart/bar";
 import { DoughnutChart } from "@/components/chart/doughnut";
 
 function Warehouse() {
   return (
     <div>
       <AppHeader title={"Dashboard"} />
-      <LineChart />
-      <AreaChart />
-      <DoughnutChart />
+      <div className="grid grid-cols-2 gap-2">
+        <div className=" justify-self-center">
+          <LineChart />
+        </div>
+        <div className=" justify-self-center">
+          <AreaChart />
+        </div>
+        <div className=" justify-self-center">
+          <BarChart />
+        </div>
+        <div className=" justify-self-center">
+          <DoughnutChart />
+        </div>
+      </div>
     </div>
   );
 }
