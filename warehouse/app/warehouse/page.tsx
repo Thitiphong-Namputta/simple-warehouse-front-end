0adb9f191bf21256441c7ef9ef1f871ec1639ece
big <thitiphong479big@hotmail.com>
2026-01-17 20:15:30 +0700

    add chart js and faker js

--- warehouse/app/warehouse/page.tsx
+++ warehouse/app/warehouse/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { AppHeader } from "@/components/layouts/app-header";
+import { LineChart } from "@/components/chart/line";
+import { AreaChart } from "@/components/chart/area";
+import { DoughnutChart } from "@/components/chart/doughnut";
 
 function Warehouse() {
   return (
     <div>
       <AppHeader title={"Dashboard"} />
+      <LineChart />
+      <AreaChart />
+      <DoughnutChart />
     </div>
   );
 }
